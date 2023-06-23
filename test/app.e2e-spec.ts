7c610e346ebdc8868f52debfa1c5f8576b374eb6
ToluOjo <ojotolulpe137@gmail.com>
2023-06-23 13:32:31 +0100

    setting up test db

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import{ValidationPipe} from '@nestjs/common'
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('App e2e', () => {
+  let app;
+  beforeAll(async () => {
+    const moduleRef =
+      await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+     app = moduleRef.createNestApplication();
+     app.useGlobalPipes(new ValidationPipe({
+      whitelist:true, 
+    })
+    ); 
+  await app.init();
+  }); 
+  afterAll(() =>{
+  app.close(); 
+  })
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  it.todo('should pass');
 });
